2421bbfd0a316b00fd254a2517f9a06c82c1c536
galinabelyaeva <belyaeva.galya@gmail.com>
2017-06-27 10:18:11 -0700

    feature file

---
+++ features/instructions.feature
@@ -0,0 +1,94 @@
+
+
+  Feature: Regression Test
+
+
+    Scenario: Go to Amazon homepage
+      Then Go to Amazon homepage
+
+    Scenario: Make sure user is able to add item to the cart
+      Then Launch Toysrus website
+      Then Click the category Age
+      Then Click the subcategory Big Kids
+      Then Select the 1 item on the page
+      Then Click the cartAddition button
+      Then Verify items added to the cart: 1
+
+    Scenario: Make sure user is able to add item to the cart
+      Then Launch Toysrus website
+      Then Click the category Age
+      Then Click the subcategory Big Kids
+      Then Select the 2 item on the page
+      Then Click the cartAddition button
+      Then Verify items added to the cart: 2
+
+
+#    Scenario: Make sure user is able to add item to the cart from Category Boys' Toys
+#      Then Launch Toysrus website
+#      Then Click the subcategory Boys' Toys
+#      Then Click the subcategory Action Figures
+#      Then Select the 1 item on the page
+#      Then Click the cartAddition button
+#      Then Verify items added to the cart: 2
+#
+#    Scenario: Make sure user is able to add item to the cart from Category Girls' Toys
+#      Then Launch Toysrus website
+#      Then Click the subcategory Girls' Toys
+#      Then Click the subcategory Arts & Crafts
+#      Then Select the 1 item on the page
+#      Then Click the cartAddition button
+#      Then Verify items added to the cart: 3
+
+
+    Scenario: Make sure user is able to add item to the cart from Category Outdoor Play
+        Then Launch Toysrus website
+        Then Click the category Outdoor Play
+        Then Click the subcategory Swing Sets
+        Then Click the subcategory Swings
+        Then Select the 1 item on the page
+        Then Click the cartAddition button
+        Then Click Shop by button
+        Then Click the category Outdoor Play
+        Then Click the subcategory Climbers
+        Then Select the 1 item on the page
+        Then Click the cartAddition button
+        Then Verify items added to the cart: 4
+
+    Scenario: Make sure user is able to add item to the cart from subcategory 2-4 Years
+      Then Launch Toysrus website
+      Then Click the category Age
+      Then Click the subcategory Big Kids
+      Then Select the 3 item on the page
+      Then Click the cartAddition button
+      Then Verify items added to the cart: 5
+
+
+
+#    Scenario: Make sure user is able to add item to the cart from Gift Finder (iframe)
+#      Then Launch Toysrus website
+#      Then Navigate to Gift Finder tab
+#      Then Verify the gift finder
+
+#      What does this do?
+
+#      Then /^How to deal with iframes$/ do
+#    $driver.switch_to.frame "onmiTest" # this is iframe's id, we are entering the iframe
+#    $driver.find_element :xpath => "//span[@class = 'hideSmall']"
+#    $driver.switch_to.default_content # we are exiting the iframe
+
+
+  #Feature: Acceptance Test
+  #
+  #  Scenario: Login
+  #    Then Login into Facebook
+
+  #  1. Add 2 item to the cart for category Outdoor Play from different subcategories
+  #  2. Add 2 identical item to the cart for category Clearance
+  #  3. Add 1 item from the category Age and 1 item from the category Outdoor Play
+  #  4. Remove item from the Cart
+
+
+    #App Mimo
+    #Lesson 2 test cases DONE!
+    #Lesson 3 test cases DONE!
+    #Read on lessons 4-5 DONE!
---
+++ features/steps_definitions/steps.rb
@@ -0,0 +1,198 @@
+
+Then /^Go to Amazon homepage$/ do
+  driver = $driver
+  driver.get "http://www.amazon.com/"
+end
+
+=begin
+LOCATORS: xpath, id, css, link, name and DOM.
+Feature and scenario are reserved words
+driver is how we call Selenium WebDriver
+
+element = $driver.find_element :xpath => ""
+element = $driver.find_element :id => ""
+
+
+Then /^Login into Facebook$/ do
+  driver = $driver
+  driver.get "http://www.facebook.com/"
+end
+
+=end
+
+=begin
+Then /^Launch Toysrus website$/ do
+  $driver.get "http://www.toysrus.com/"
+  $driver.manage.window.maximize
+  sleep 3
+end
+=end
+
+
+Then /^Launch Toysrus website$/ do
+  $driver.get $basicurl
+  $driver.manage.window.maximize
+  sleep 3
+end
+
+
+
+Then /^Click the category Age$/ do
+  element = $driver.find_element :xpath => "//a[@class = ' mainNavLink mainNavLink2' and text() = 'Age']"
+  element.click
+  sleep 3
+end
+
+Then /^Click the category Outdoor Play$/ do
+  element = $driver.find_element :xpath => "//a[@class = ' mainNavLink mainNavLink11' and text() = 'Outdoor Play']"
+  element.click
+  sleep 3
+end
+
+
+
+Then /^Click the subcategory ([^"]*)$/ do |subcategory_name| # subcategory_name is variable name, ([^"]*) is variable value that's passed into the variable name
+  # element = $driver.find_element :xpath => "//div[@class = 'sliderWrapper']/p/a[text() = '#{subcategory_name}']"
+  element = $driver.find_element :xpath => "//div[@class = 'sliderWrapper']/p/a[contains(text(),'#{subcategory_name}')]"
+  element.click
+  sleep 3
+end
+
+Then /^Click the subcategory Boys' Toys$/ do
+  element = $driver.find_element :xpath => "//a[@class = ' mainNavLink mainNavLink0' and text() = \"Boys' Toys\"]" #treat single quote as a regular character
+  element.click
+  sleep 3
+end
+
+Then /^Click the subcategory Girls' Toys$/ do
+  element = $driver.find_element :xpath => "//a[@class = ' mainNavLink mainNavLink1' and text() = \"Girls' Toys\"]" #treat single quote as a regular character
+  element.click
+  sleep 3
+end
+
+=begin
+Then /^Click the subcategory Swing Sets$/ do
+  element = $driver.find_element :xpath => "//div[@class = 'sliderWrapper']/p/a[text() = 'Swing Sets']"
+  element.click
+  sleep 5
+end
+
+
+Then /^Click the subcategory Swings$/ do
+  element = $driver.find_element :xpath => "//div[@class = 'sliderWrapper']/p/a[text() = 'Swings']"
+  element.click
+  sleep 5
+end
+
+Then /^Click the subcategory Climbers$/ do
+  element = $driver.find_element :xpath => "//div[@class = 'sliderWrapper']/p/a[text() = 'Climbers']"
+  element.click
+  sleep 5
+end
+
+Then /^Click the subcategory Action Figures$/ do
+  element = $driver.find_element :xpath => "//div[@class = 'sliderWrapper']/p/a[text() = 'Action Figures']"
+  element.click
+  sleep 5
+end
+
+Then /^Click the subcategory Arts & Crafts$/ do
+  element = $driver.find_element :xpath => "//div[@class = 'sliderWrapper']/p/a[text() = 'Arts & Crafts']"
+  element.click
+  sleep 5
+
+end
+=end
+
+Then /^Select the ([^"]*) item on the page$/ do |item_by_index|
+  element = $driver.find_element :xpath => "(//img[@class = 'swatchProdImg'])['#{item_by_index}']"
+  element.click
+  sleep 3
+end
+
+=begin
+Then /^Click the Add to Cart button$/ do
+  element = $driver.find_element :id => "cartAddition"
+  element.click
+  sleep 5
+end
+=end
+
+
+Then /^Click the ([^"]*) button$/ do |button|  # In cucumber file, we have to name the button by its id so that the variable value is passed. For example: cartAddition.
+  element = $driver.find_element :id => button
+  element.click
+  sleep 3
+end
+
+
+Then /^Click Shop by button$/ do
+  element = $driver.find_element :xpath => "//a[@class = 'navButton']"
+  element.click
+  sleep 3
+end
+
+Then /^Verify items added to the cart: ([^"]*)$/ do |number_of_items|
+  $driver.find_element :xpath => "//span[@class = 'cartItemCount' and text() = '#{number_of_items}']"
+end
+
+=begin
+Then /^Verify items added to the cart: 2$/ do
+  $driver.find_element :xpath => "//span[@class = 'cartItemCount' and text() = '2']"
+end
+
+Then /^Verify items added to the cart: 3$/ do
+  $driver.find_element :xpath => "//span[@class = 'cartItemCount' and text() = '3']"
+end
+=end
+
+
+Then /^Navigate to Gift Finder tab$/ do
+  element = driver.find_element :xpath => "//li[@class = 'giftFinder headerTip']/a[contains(text(), 'Gift Finder')]"
+  element.click
+  sleep 3
+end
+
+
+Then /^Verify the gift finder$/ do
+  $driver.switch_to.frame "onmiTest" #iframe's id
+  $driver.find_element :xpath => "//span[@class = 'hideSmall']"
+  $driver.switch_to.default_content
+
+end
+
+=begin
+Then /^Click the subcategory 2-4 Years$/ do
+  #element = $driver.find_element :xpath => "//div[@class = 'sliderWrapper']/p/a[text()= '2-4 Years']"
+  element = $driver.find_element :xpath => "//div[@class = 'sliderWrapper']/p/a[contains(text(),'2-4 Years')]" # syntax for additional tags in the middle
+  #element = driver.find_element :xpath => "//a[@class='hdrMenuCat'][contains(text(),'2 - 4 Years')]" syntax for no additional tags in the middle
+  element.click
+
+end
+=end
+
+=begin
+
+CONTAINS TEXT XPATH $x("//div[@class = 'sliderWrapper']/p/a[contains(text(), 'Big')]")
+
+Embedded tags: /p/a - going level down
+
+ABSOLUTE VS RELEVANT XPATH
+
+Relevant makes the tests run faster, it's simple, elegant and gives us better idea of the element.
+Absolute - unstable because HTML structure can change, makes the tests run slower.
+
+
+IFRAMES
+All iframes have ids
+
+Then /^How to deal with iframes$/ do
+  $driver.switch_to.frame "onmiTest" # this is iframe's id, we are entering the iframe
+  $driver.find_element :xpath => "//span[@class = 'hideSmall']"
+  $driver.switch_to.default_content # we are exiting the iframe
+
+end
+
+
+=end
+
